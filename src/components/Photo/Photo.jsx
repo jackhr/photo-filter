cee91df848ae5dd10b1e47d2bebafb737cf76148
jackhr <jc2o@mac.com>
2021-05-10 16:14:00 -0400

    stub up Photo component

---
+++ src/components/Photo/Photo.jsx
@@ -0,0 +1,11 @@
+import { Link } from 'react-router-dom';
+import * as userService from '../../utilities/users-service';
+
+export default function Photo({ photo }) {
+
+  return (
+    <div>
+      <h1>This is a photo component</h1>
+    </div>
+  );
+}

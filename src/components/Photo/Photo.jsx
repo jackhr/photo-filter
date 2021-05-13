5a6ec2896b246aaba80107e2f35a4c9c0595b8e7
jackhr <jc2o@mac.com>
2021-05-12 21:35:14 -0400

    fix incorrect url for uploading to AWS S3

--- src/components/Photo/Photo.jsx
+++ src/components/Photo/Photo.jsx
@@ -1,11 +1,16 @@
 import { Link } from 'react-router-dom';
 import * as userService from '../../utilities/users-service';
+import './Photo.css'
 
 export default function Photo({ photo }) {
+  function testerz() {
+    console.log(photo.imageURL);
+  }
 
   return (
     <div>
       <h1>{photo.name}</h1>
+      <img className="display-image" onClick={testerz} src={photo.imageURL} />
     </div>
   );
 }
